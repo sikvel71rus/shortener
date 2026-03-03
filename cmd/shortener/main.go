633cbe3efead08942204e200a457a2fcb0a24d8b
Юбин Никита Владиславович <jubin2la@gmail.com>
2026-03-03 16:07:16 +0300

    iter3

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/go-chi/chi/v5"
 	"github.com/sikvel71rus/shortener.git/internal/handler"
 	"github.com/sikvel71rus/shortener.git/internal/repository"
 	"github.com/sikvel71rus/shortener.git/internal/service"
@@ -13,11 +14,12 @@ func main() {
 	srv := service.NewURLService(repo)
 	h := handler.NewURLHandler(srv, "http://localhost:8080")
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("POST /", h.PostHandler)
-	mux.HandleFunc("GET /{id}", h.GetHandler)
+	r := chi.NewRouter()
 
-	err := http.ListenAndServe(":8080", mux)
+	r.Post("/", h.PostHandler)
+	r.Get("/{id}", h.GetHandler)
+
+	err := http.ListenAndServe(":8080", r)
 	if err != nil {
 		panic(err)
 	}
