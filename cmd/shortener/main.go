47be8e9facf5ec55faa6f8e4829d93e3e20efabc
Юбин Никита Владиславович <jubin2la@gmail.com>
2026-03-21 16:30:24 +0300

    iter6: add logger.go

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -4,8 +4,10 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/sikvel71rus/shortener.git/internal/config/starter"
 	"github.com/sikvel71rus/shortener.git/internal/handler"
+	"github.com/sikvel71rus/shortener.git/internal/logger"
 	"github.com/sikvel71rus/shortener.git/internal/repository"
 	"github.com/sikvel71rus/shortener.git/internal/service"
+	"log"
 	"net/http"
 )
 
@@ -13,11 +15,19 @@ func main() {
 
 	starterCfg := starter.Parse()
 
+	if err := logger.Initialize("info"); err != nil {
+		panic(err)
+	}
+
 	repo := repository.NewMapURLRepo()
 	srv := service.NewURLService(repo, starterCfg.BaseURL)
 	h := handler.NewURLHandler(srv)
 
 	r := chi.NewRouter()
+
+	log.Printf("Сервер запущен на %s, базовый адрес: %s", starterCfg.ServerAddress, starterCfg.BaseURL)
+
+	r.Use(logger.RequestLogger)
 
 	r.Post("/", h.PostURLHandler)
 	r.Get("/{id}", h.GetURLHandler)
