10ca7cc7ac8bc7ffecea55340b866433e686583d
Юбин Никита Владиславович <jubin2la@gmail.com>
2026-04-30 01:24:12 +0300

    iter14

--- internal/repository/migrations/00003_add_user_urls.sql
+++ internal/repository/migrations/00003_add_user_urls.sql
@@ -1,7 +1,7 @@
 -- +goose Up
 CREATE TABLE IF NOT EXISTS user_urls (
     user_id VARCHAR(255) NOT NULL,
-    short_id VARCHAR(255) NOT NULL REFERENCES shortener(short_id),
+    short_id VARCHAR(255) NOT NULL,
     PRIMARY KEY (user_id, short_id)
 );
 
